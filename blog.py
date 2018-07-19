1c0062e512001301b5d506276929f99fe2537a49
Liam Graham <liam.david.graham@gmail.com>
2018-07-19 22:31:22 +1000

    Add doc string

--- blog.py
+++ blog.py
@@ -1,56 +1,135 @@
-from os import listdir, path
+from os import listdir, path, makedirs
+from shutil import rmtree
+from dataclasses import dataclass
 
+# Paths for blog, blog post text, and template directories.
 BLOG_DIR = "blog"
 TEXT_DIR = "blog\\_text"
-TEMPLATE_DIR = "templates\\post.html"
+TEMPLATE_DIR = "templates"
 
 
-def get_posts():
-    post_names = listdir(TEXT_DIR)
-    return post_names
+def get_text_names():
+    """
+    Get names of all blog post text files.
+
+    Returns (list(str)): names of all blog post text files
+    """
+    text_names = listdir(TEXT_DIR)
+    return text_names
 
 
-def write_static(post_name):
-    text = get_post_content(post_name)
-    static = get_template()
+def write_post_static(text_name):
+    """
+    Creates a static HTML file for the blog post having the given text name.
 
-    name = path.splitext(post_name)[0]
+    Arguments:
+        text_name: name of the blog post text file
 
+    Returns (BlogPost): blog post object
+    """
+    text = get_post_content(text_name)
+    static = get_template("post")
+
+    full_name = path.splitext(text_name)[0].split("-")
+    date = full_name[0:3]
+    file_name = f"{'-'.join(full_name[3:])}.html"
+    title, content = format_content(text)
+
+    static = static.replace("{ POST_TITLE }", title)
+    static = static.replace("{ POST_DATE }", "-".join(date))
+    static = static.replace("{ POST_CONTENT }", content)
+
+    date_path = "\\".join(date)
+    post_dir = f"{BLOG_DIR}\\{date_path}"
+    if not path.exists(post_dir):
+        makedirs(post_dir)
+
+    with open(f"{post_dir}\\{file_name}", "w") as f:
+        f.write(static)
+    return BlogPost(title, date, content, post_dir)
+
+
+def format_content(text):
+    """
+    Returns the given text formatted for display on a webpage. Paragraph tags are
+    inserted so that paragraph breaks are retained. 
+
+    Arguments:
+        text (str): text to be formatted
+
+    Returns (str): formatted text
+    """
     content = ""
     paragraph = ""
+    title = ""
     lines = text.split("\n")
-    for line in lines:
-        line = line.strip()
-        if not line:
+    title = lines.pop(0)
+    for x in lines:
+        line = x.strip()
+        if line:
+            paragraph += line
+        else:
             content += f"<p>{paragraph}</p>"
             paragraph = ""
-        else:
-            paragraph += line
+    return title, content
 
-    static = static.replace("{POST_TITLE}", name)
-    static = static.replace("{POST_DATE}", "2018-07-18")
-    static = static.replace("{POST_CONTENT}", content)
-    
-    with open(f"{BLOG_DIR}\\{name}.html", "w") as f:
+
+def write_blog_static(posts):
+    """
+
+
+    """
+    static = get_template("blog")
+    post_list = ""
+
+    for x in posts:
+        post_list += f"<li><a href='{x.directory}'>{x.date} - {x.title}</a></li>\n"
+    static = static.replace("{ POST_LIST }", post_list)
+
+    with open("blog.html", "w") as f:
         f.write(static)
 
 
-def write_list():
-    pass
+def get_post_content(text_name):
+    """
+    Returns the content of the blog post text having the given name.
 
-
-def get_post_content(post_name):
+    Arguments: 
+        text_name (str): name of blog post text
+    
+    Returns (str): content of target blog post text
+    """
     with open(f"{TEXT_DIR}\\{post_name}", "r") as f:
         return f.read()
 
 
-def get_template():
-    with open(TEMPLATE_DIR, "r") as f:
+def get_template(name):
+    """
+    Returns the text of the HTML template having the given name.
+
+    Arguments:
+        name (str): name of template, excluding the file extension
+
+    Returns (str): text of HTML template
+    """
+    with open(f"{TEMPLATE_DIR}\\{name}.html", "r") as f:
         return f.read()
 
 
+@dataclass
+class BlogPost:
+    """
+    Data class storing all blog post data.
+    """
+    title: str
+    date: str
+    content: str
+    directory: str
+
+
 if __name__ == "__main__":
-    post_names = get_posts()
+    post_names = get_text_names()
+    posts = []
     for name in post_names:
-        write_static(name)
-    write_list()
+        posts.append(write_post_static(name))
+    write_blog_static(posts)
